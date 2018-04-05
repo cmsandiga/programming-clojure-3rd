27540e96646cfb630ac375408a647120a06c972e
Cristian Makoto <cmsandiga@gmail.com>
2018-04-05 02:52:31 +0000

    repeat

--- src/programming_clojure/sequence.clj
+++ src/programming_clojure/sequence.clj
@@ -10,18 +10,35 @@
   (println (conj '(1 2 3) 20))
   (println (seq? (rest [1 2 3])))
   )
-(defn mapas []
+(defn maps []
   (println {:fname "Aaron" :lname "Bedra"})
   (println (first {:fname "Aaron" :lname "Bedra"}))
   (println (rest {:fname "Aaron" :lname "Bedra"}))
-  (println (cons [:mname "james"]  {:fname "Aaron" :lname "Bedra"}))
+  (println (cons [:mname "james"] {:fname "Aaron" :lname "Bedra"}))
   )
 
-(defn seq [])
+(defn sets []
+  (println (first #{:the :quick :brown :fox}))
+  (println (rest #{:the :quick :brown :fox}))
+  (println (cons :jumped #{:the :quick :brown :fox}))
+  (println (conj #{:the :quick :brown :fox} :jumped))
+  (println (sorted-set :the :quick :brown :fox :a :b :z :d :f :y :w))
+  )
+
+(defn sequences []
+  (println (range 10))
+  (println (range 10 20 2))
+  )
+
+(defn repeat-test []
+    (println (repeat 20 "20"))
+  )
 (defn -main []
   (println (myfun))
   (println (vectors))
-  (println (mapas))
-
+  (println (maps))
+  (println (sets))
+  (println (sequences))
+  (repeat-test)
   )
 
